754c918c1dacd8e3199e4e00c6837c7aeec3a452
AH-Bagheri <whitewolf97@yahoo.com>
2024-08-20 11:01:53 +0330

    Add Some Signed Numbers From Flash Buffer Using LDRSB Mnemonic

--- MDK-ARM/test.s
+++ MDK-ARM/test.s
@@ -1,34 +1,37 @@
 
             ; In The Name Of GOD  ;
-BUF_SIZE    EQU ((__data_e - __data_s) / 4)        
+INDEX_INC   EQU 1            
+BUF_SIZE    EQU ((__data_e - __data_s) / INDEX_INC)        
 
-COUNTER     RN  R0
-MAX         RN  R1
+SUM         RN  R0
+COUNTER     RN  R1
 ITEM        RN  R2
 POINTER     RN  R3
         
             AREA myData, DATA, READONLY, ALIGN=4
-__data_s    DCD 0x15, 0x10, 0x30, 0x20
+__data_s    DCB -1, -2, -3, -4, 10, -1
 __data_e  
             
             
             AREA myCode, CODE, READONLY
             EXPORT __test
             
-__test      MOV   MAX,     #0       
+__test      MOV   SUM,     #0
             LDR   COUNTER, =BUF_SIZE
             CMP   COUNTER, #0
             BEQ   __end
             LDR   POINTER, =__data_s
             
-__again     LDR   ITEM, [POINTER], #4
-            CMP   MAX, ITEM
-            BHS   __index
-            MOV   MAX, ITEM
-            
-__index     SUBS  COUNTER, COUNTER, #1
+__again     LDRSB ITEM, [POINTER], #INDEX_INC   
+            ADD   SUM, ITEM
+            SUBS  COUNTER, COUNTER, #1
             BNE   __again
             
 __end       BX  LR
             ALIGN
             END
+              
+              
+              
+              
+              
