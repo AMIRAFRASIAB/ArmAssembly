55c5b94ad8e65e3ef9e489a573a0e583042ed63b
AH-Bagheri <whitewolf97@yahoo.com>
2024-08-21 09:23:50 +0330

    Find The Min Signed Byte

--- MDK-ARM/test.s
+++ MDK-ARM/test.s
@@ -3,30 +3,33 @@
 INDEX_INC   EQU 1            
 BUF_SIZE    EQU ((__data_e - __data_s) / INDEX_INC)        
 
-SUM         RN  R0
+MIN         RN  R0
 COUNTER     RN  R1
 ITEM        RN  R2
 POINTER     RN  R3
         
             AREA myData, DATA, READONLY, ALIGN=4
-__data_s    DCB -1, -2, -3, -4, 10, -1
+__data_s    DCB -1, -2, -3, -4, 10, -9
 __data_e  
             
             
             AREA myCode, CODE, READONLY
             EXPORT __test
             
-__test      MOV   SUM,     #0
-            LDR   COUNTER, =BUF_SIZE
-            CMP   COUNTER, #0
-            BEQ   __end
-            LDR   POINTER, =__data_s
+__test      LDR     COUNTER,  =BUF_SIZE
+            CMP     COUNTER,  #0
+            BEQ     __end     
+            LDR     POINTER,  =__data_s
+            LDRSB   MIN,      [POINTER]
             
-__again     LDRSB ITEM, [POINTER], #INDEX_INC   
-            ADD   SUM, ITEM
-            SUBS  COUNTER, COUNTER, #1
-            BNE   __again
+__again     LDRSB   ITEM,     [POINTER], #1
+            CMP     ITEM,     MIN
+            BGE     __jump
+            MOV     MIN,      ITEM
             
+__jump      SUBS    COUNTER,  COUNTER, #1
+            BNE     __again
+
 __end       BX  LR
             ALIGN
             END
